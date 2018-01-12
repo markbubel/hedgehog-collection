4010f461d4d7a7532dd03a09d7dd49286aaa4e0b
Mark <mark.bubel@gmail.com>
2018-01-12 17:44:05 -0500

    Updating game

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-let flex = document.querySelector('.flex-box');
-let allBoxes = []; // the array of hedgehogs
+let hedgehogContainer = document.querySelector('.flex-box');
+let hedgehogNodes = []; // the array of hedgehogs
 
 // Adding hedgehogs
 let submitButton = document.querySelector('.submit-btn');
 submitButton.addEventListener("click", function (e) {
     e.preventDefault();
-    if (allBoxes.length >= 1) {
-        // Removing hedgehogs
-        for (let i = 0, len = allBoxes.length; i < len; i++) {
-            allBoxes[i].remove();
-            console.log(allBoxes[i] + " was removed.");
+    if (hedgehogNodes.length >= 1) { // remove the hedgehogs from DOM
+        for (let i = 0; i < hedgehogNodes.length; i++) {
+            hedgehogContainer.removeChild(hedgehogNodes[i]);
         }
+        hedgehogNodes = []; // clear the array
     }
 
     let numberOfHogsField = document.getElementById('numOfHogs');
     let number = numberOfHogsField.value;
-
+    console.log(number);
     while (number > 0) {
         let box = document.createElement('div');
-        allBoxes.push(box);
+        hedgehogNodes.push(box);
         box.setAttribute('class', 'box');
-        flex.appendChild(box);
-        number--
+        hedgehogContainer.appendChild(box);
+        number--;
     }
 });
 
