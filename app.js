acf6682fe5368e3ca93ecf6658e35f742ef609c2
Mark <mark.bubel@gmail.com>
2018-01-12 22:06:32 -0500

    Adding won message

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let hedgehogContainer = document.querySelector('.flex-box');
 let hedgehogNodes = []; // the array of hedgehogs
+let scoreCount = 1;
 
 // Adding hedgehogs
 let submitButton = document.querySelector('.submit-btn');
@@ -9,21 +10,41 @@ submitButton.addEventListener("click", function (e) {
         for (let i = 0; i < hedgehogNodes.length; i++) {
             hedgehogContainer.removeChild(hedgehogNodes[i]);
         }
-        hedgehogNodes = []; // clear the array
+        hedgehogNodes = []; // clear the hedgehog array
     }
 
+
+    // Collecting hedgehogs
     let numberOfHogsField = document.getElementById('numOfHogs');
     let number = numberOfHogsField.value;
     console.log(number);
     while (number > 0) {
         let box = document.createElement('div');
-        hedgehogNodes.push(box);
         box.setAttribute('class', 'box');
+        box.setAttribute('id', 'hh-' + number.toString()); // creating unique IDs
+        hedgehogNodes.push(box);
         hedgehogContainer.appendChild(box);
+        box.addEventListener('click', function () {
+            let scoreElement = document.getElementById('hedgehogScore');
+            scoreElement.innerHTML = 'You collected ' + scoreCount + ' hedgehogs!';
+            box.remove();
+            let index = hedgehogNodes.indexOf(this);
+            hedgehogNodes.splice(index, 1);
+            console.log("The array length is: " + hedgehogNodes.length);
+            scoreCount = scoreCount + 1;
+
+            let wonMessage = document.getElementById('won');
+            if (hedgehogNodes.length < 1) {
+                wonMessage.style.display = "block";
+            }
+
+        });
         number--;
     }
 });
 
 
 
+function moveHogs() {
 
+}
